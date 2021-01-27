aa4a8aecdb0f336370f9bebd02a552b67f08ce40
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2021-01-27 23:54:22 +0900

    Create binary_serach.py

---
+++ アルゴリズムコード/binary_serach.py
@@ -0,0 +1,21 @@
+# 二分探索アルゴリズムコード
+
+# 作成中コード
+
+import random
+
+def bin_search(nums):
+      l,r = 0,len(nums)-1
+      m = (len(nums)+1)//2
+      
+      #if nums[m] == 
+      
+
+n = 32
+a =  sorted([770, 659, 765, 416, 901, 543, 655, 171, 981, 522, 677,
+      386,  56, 782, 679, 470, 126, 649, 225, 991, 255, 121,
+      194,  91, 565, 423, 763, 554, 666, 684, 739, 677])
+#a = [random.randint(1,1000) for _ in range(n)] 
+idx = random.randint(0,n-1)
+
+print(a)
