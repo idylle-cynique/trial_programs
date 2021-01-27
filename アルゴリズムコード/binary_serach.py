27512e1f6fb190b23bcb516e58b2f32d6884add5
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2021-01-28 00:04:41 +0900

    Update binary_serach.py

--- アルゴリズムコード/binary_serach.py
+++ アルゴリズムコード/binary_serach.py
@@ -1,14 +1,20 @@
 # 二分探索アルゴリズムコード
 
-# 作成中コード
-
 import random
 
-def bin_search(nums):
+def bin_search(nums,n):
+      print(nums)
       l,r = 0,len(nums)-1
-      m = (len(nums)+1)//2
+      m = len(nums)//2
+      if len(nums) == 1 and nums[0] != n:
+            return False
       
-      #if nums[m] == 
+      if nums[m] == n:
+            return True
+      elif nums[m] < n:
+            return bin_search(nums[m+1:],n)
+      else:
+            return bin_search(nums[l:m] ,n)
       
 
 n = 32
@@ -18,4 +24,5 @@ a =  sorted([770, 659, 765, 416, 901, 543, 655, 171, 981, 522, 677,
 #a = [random.randint(1,1000) for _ in range(n)] 
 idx = random.randint(0,n-1)
 
-print(a)
+print("Is there",a[idx],"in list-a?")
+print(bin_search(a,a[idx]))
