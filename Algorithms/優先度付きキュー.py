827560d62dc73a3fbe775a38e6c7908cf967513d
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2022-05-07 22:49:29 +0900

    リポジトリ再構成のため旧ファイル群を削除

--- Algorithms/優先度付きキュー.py
+++ Algorithms/優先度付きキュー.py
@@ -6,7 +6,7 @@ class PriorityQueue:
     
     def set_values(self,input_list)-> None:
         '''リストをキューにセットしてヒープ化'''
-        self.priority_queue = input_list
+        self.priority_queue = input_list[:]
         self.length = len(self.priority_queue)
         self.build_heap()
     
@@ -34,12 +34,20 @@ class PriorityQueue:
             self.heapify((idx-1)//2)
         return 
 
+    def down_heapify(self): return
+    
+    def up_heapidy(self): return
+
     
     def build_heap(self,flag=False):
         '''ヒープを構築する 計算量: O(要素数)'''
         for index in range(len(self.priority_queue)//2,-1,-1):
             self.heapify(index)
         return 
+
+    def heappop(self):
+        self.priority_queue[0],self.priority_queue[-1] = self.priority_queue[-1],self.priority_queue[0]
+        self.priority_queue.pop()
 
     def __repr__(self):
         '''print()でキューの中身を閲覧'''
@@ -51,15 +59,9 @@ class PriorityQueue:
 def main():
     sample_array = [1,9,8,2,3,10,14,7,16,4]
     heapq = PriorityQueue()
-
-    # 処理前の優先度付きキュー
     heapq.set_values(sample_array)
+    print(sample_array)
     print(heapq)
-
-    # ヒープ化後の優先度付きキュー
-    heapq.build_heap()
-    print(heapq)
-    
     return
 
 
