32bfaea9ece255932940d085153d8a1d55c08349
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2022-06-09 21:19:36 +0900

    Update my_heapq.py
    heapsort()を追加

--- Algorithms/my_heapq.py
+++ Algorithms/my_heapq.py
@@ -18,6 +18,18 @@ class PriorityQueue:
         ret_ele = self.priorityqueue[0]
         heapq.heappop(self.priorityqueue)
         return ret_ele
+
+    def heapsort(self)->list:
+        sorted_list = list()
+        pq = list()
+
+        while(self.priorityqueue):
+            min_ele = heapq.heappop(self.priorityqueue)
+            sorted_list.append(min_ele)
+            heapq.heappush(pq,min_ele)
+
+        self.priorityqueue = pq
+        return sorted_list
     
     def __repr__(self)-> str:
         return f"PriorityQueue({self.priorityqueue})"
@@ -29,10 +41,11 @@ def main():
     length = 32
     numbers = [random.randint(1,100) for x in range(length)]
     numbers = [52, 46, 18, 59, 58, 40, 77, 39, 69, 46, 74, 39, 29, 59, 62, 64, 57, 39, 35, 92, 39, 57, 64, 35, 33, 31, 57, 71, 36, 3, 83, 57]
-    print(numbers)
-
     hq = PriorityQueue(numbers)
+
+    print(numbers)
     print(hq)
+    print(hq.heapsort())
     return 
 
 
